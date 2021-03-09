931e1b4a8a9d4355bc729d4bd92f52aa20eff3b9
RTSGame <Nanoa57@yandex.ru>
2021-03-09 17:09:55 +0300

    T1

---
+++ Cube_Moving.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube_Moving : MonoBehaviour
+{
+    public Vector3 StartPos, NewPos;//NewPos Установка от PZ
+    public float Timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Timer < 1.5f)
+        {
+            Timer += Time.deltaTime;
+            transform.position = Vector3.Lerp(StartPos, NewPos, Timer);
+            if (Timer >= 1)//Дожим
+            {
+                transform.position = NewPos;
+            }
+        }
+
+
+    }
+}
